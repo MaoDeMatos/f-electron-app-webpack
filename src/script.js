a8acd1f966084447af6ad2d0936dc89207f4a296
Mao De Matos <mao.dematos@gmail.com>
2022-05-11 16:48:56 +0200

    ♻️ Refactor main script to use it during tests

--- src/script.js
+++ src/script.js
@@ -1,77 +1,91 @@
-window.onload = () => {
-  let steps, numberToFind;
+let steps, numberToFind;
 
-  // const numberToFindHtmlElement = document.getElementById("numberToFind");
-  const numberInputHtmlElement = document.getElementById("number-input");
-  const alreadyTriedHeaderHtmlElement = document.getElementById(
-    "already-tried-header"
-  );
-  const alreadyTriedHtmlElement = document.getElementById("already-tried");
-  const messageHtmlElement = document.getElementById("message");
+const numberInputHtmlElement = document.getElementById("number-input");
+const alreadyTriedHeaderHtmlElement = document.getElementById(
+  "already-tried-header"
+);
+const alreadyTriedHtmlElement = document.getElementById("already-tried");
+const messageHtmlElement = document.getElementById("message");
 
-  const resetButton = document.getElementById("reset-button");
-  resetButton.onclick = () => initGuessTheNumber();
+const resetButton = document.getElementById("reset-button");
+resetButton.onclick = () => initGuessTheNumber();
 
-  const formInputHtmlElement = document.getElementById("number-form");
-  formInputHtmlElement.onsubmit = (e) => {
-    e.preventDefault();
-    const inputValue = parseInt(e.target[0].value);
-    e.target[0].value = null;
-
-    let won = false;
-
-    if (!inputValue) return;
-    else if (inputValue === numberToFind) {
-      youWon();
-      won = true;
-    } else if (inputValue > numberToFind) yourInputWasGreater();
-    else if (inputValue < numberToFind) yourInputWasSmaller();
-
-    steps++;
-
-    if (!alreadyTriedHeaderHtmlElement.innerHTML)
-      alreadyTriedHeaderHtmlElement.innerHTML = "Already tried :";
-
-    const li = document.createElement("li");
-    li.innerHTML = inputValue;
-    alreadyTriedHtmlElement.appendChild(li);
-
-    if (steps > 4 && !won) {
-      youLost();
-    }
-  };
-
-  function youWon() {
-    messageHtmlElement.innerHTML = "You won !";
-    numberInputHtmlElement.setAttribute("disabled", "");
-  }
-
-  function yourInputWasGreater() {
-    messageHtmlElement.innerHTML = "It's less !";
-  }
-
-  function yourInputWasSmaller() {
-    messageHtmlElement.innerHTML = "It's more !";
-  }
-
-  function youLost() {
-    messageHtmlElement.innerHTML =
-      "You lost ! The number was : " + numberToFind;
-    numberInputHtmlElement.setAttribute("disabled", "");
-  }
-
-  function initGuessTheNumber() {
-    steps = 0;
-    numberToFind = getRandomNumInRange(0, 50);
-
-    numberInputHtmlElement.removeAttribute("disabled");
-    messageHtmlElement.innerHTML = "";
-    alreadyTriedHeaderHtmlElement.innerHTML = "";
-    alreadyTriedHtmlElement.innerHTML = "";
-  }
-
-  initGuessTheNumber();
+const formInputHtmlElement = document.getElementById("number-form");
+formInputHtmlElement.onsubmit = (e) => {
+  handleSubmit(e);
 };
+
+initGuessTheNumber();
+
+//
+// Functions
+//
+
+function handleSubmit(e) {
+  e.preventDefault();
+  const inputValue = parseInt(e.target[0].value);
+  e.target[0].value = null;
+
+  let won = false;
+
+  if (!inputValue) {
+    messageHtmlElement.innerHTML = "";
+    return;
+  } else if (inputValue > 50 || inputValue < 0) {
+    valueNotInRange();
+    return;
+  } else if (inputValue === numberToFind) {
+    youWon();
+    won = true;
+  } else if (inputValue > numberToFind) yourInputWasGreater();
+  else if (inputValue < numberToFind) yourInputWasSmaller();
+
+  steps++;
+
+  if (!alreadyTriedHeaderHtmlElement.innerHTML)
+    alreadyTriedHeaderHtmlElement.innerHTML = "Already tried :";
+
+  const li = document.createElement("li");
+  li.innerHTML = inputValue;
+  alreadyTriedHtmlElement.appendChild(li);
+
+  if (steps > 4 && !won) {
+    youLost();
+  }
+}
+
+function youWon() {
+  messageHtmlElement.innerHTML = "You won !";
+  numberInputHtmlElement.setAttribute("disabled", "");
+}
+
+function yourInputWasGreater() {
+  messageHtmlElement.innerHTML = "It's less !";
+}
+
+function yourInputWasSmaller() {
+  messageHtmlElement.innerHTML = "It's more !";
+}
+
+function youLost() {
+  messageHtmlElement.innerHTML = "You lost ! The number was : " + numberToFind;
+  numberInputHtmlElement.setAttribute("disabled", "");
+}
+
+function valueNotInRange() {
+  messageHtmlElement.innerHTML =
+    "The number must be between 0 and 50 (included).";
+}
+
+function initGuessTheNumber() {
+  steps = 0;
+  numberToFind = getRandomNumInRange(0, 50);
+
+  numberInputHtmlElement.removeAttribute("disabled");
+  messageHtmlElement.innerHTML = "";
+  alreadyTriedHeaderHtmlElement.innerHTML = "";
+  alreadyTriedHtmlElement.innerHTML = "";
+}
 
 /**
  * Generates a random number in range
